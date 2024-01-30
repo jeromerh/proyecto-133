a97a1a828911452d876117c01200da49b983a205
jeromerh <119083415+jeromerh@users.noreply.github.com>
2024-01-30 17:54:53 -0600

    Add files via upload

---
+++ cancha.js
@@ -0,0 +1,45 @@
+object_detector=""
+img=""
+objects=[]
+status=""
+
+function preload(){
+img=loadImage("cancha.webp")
+
+}
+
+function setup(){
+canvas=createCanvas(650,420)
+canvas.center()
+object_detector=ml5.objectDetector("cocossd",modelLoaded)
+document.getElementById("status").innerHTML="Status detectando objetos"
+}
+
+
+function modelLoaded(){
+    console.log("Modelo cargado")
+    status=true 
+}
+
+function gotResults(error,results){
+   if(error)[
+    console.error(error)
+   ]
+   console.log(results) 
+   objects=results
+}
+
+function draw(){
+image(img,0,0,650,420)
+if(status!=""){
+    r=random(255)
+    g=random(255)
+    b=random(255)
+    object_detector.detect(img,gotResults)
+    for(i=0;i<objects.length;i++){
+     document.getElementById("status").innerHTML="status objeto detectado"
+     document.getElementById("num_objets").innerHTML="el numero de objetos detectados es:"+objects.length
+    }
+}
+
+}
